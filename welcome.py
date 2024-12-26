1943ef67ddd239ed574e440cd927d91ca8110ae7
sammaknshinde2410 <sammakns2001@rediffmail.com>
2024-12-26 16:15:21 +0530

    updated welcome.py

--- welcome.py
+++ welcome.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "<h1>Welcome to My Flask App!</h1>"
+    return "<h1>I am Bored....will do it tomorrow!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
