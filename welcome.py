c32a2ec5b12b3954f886e70cccb8a2cd2a58fd74
sammaknshinde2410 <sammakns2001@rediffmail.com>
2024-12-27 19:20:02 +0530

    Modified welcome.py

--- welcome.py
+++ welcome.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "<h1>I am Bored....will do it tomorrow!</h1>"
+    return "<h1>Modified File</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
