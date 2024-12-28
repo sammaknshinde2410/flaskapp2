cd40c7d53af63c4d1f20d5f82bf46c053e20b734
sammaknshinde2410 <sammakns2001@rediffmail.com>
2024-12-28 11:57:43 +0530

    updated Welcome.py

--- welcome.py
+++ welcome.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "<h1>Modified File</h1>"
+    return "<h1>This is Jenkins CI CD pipeline.</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
